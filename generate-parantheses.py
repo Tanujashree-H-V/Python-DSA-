dd5b2831d773654342bd22669411635865bc1570
Tanujashree-H-V <tanujatanu354@gmail.com>
2026-06-28 22:28:56 +0530

    generate parantheses

---
+++ generate-parantheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        result = []
+
+        def backtrack(curr: str, open_count: int, close_count: int):
+            if len(curr) == 2 * n:
+                result.append(curr)
+                return
+
+            if open_count < n:
+                backtrack(curr + "(", open_count + 1, close_count)
+
+            if close_count < open_count:
+                backtrack(curr + ")", open_count, close_count + 1)
+
+        backtrack("", 0, 0)
+        return result
+    
+    
+sol = Solution()
+print(sol.generateParenthesis(3)) # ["((()))","(()())","(())()","()(())","()()()"]
+print(sol.generateParenthesis(1)) # ["()"]
