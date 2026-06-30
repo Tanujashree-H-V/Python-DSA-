ac00d86dcab5e778bd4dcab4ad6bba3453c1d8f7
Tanujashree-H-V <tanujatanu354@gmail.com>
2026-06-30 21:10:17 +0530

    finding index of the first occurance in string

---
+++ index-of-first-occurance.py
@@ -0,0 +1,19 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        n, m = len(haystack), len(needle)
+
+        # Edge case
+        if m > n:
+            return -1
+
+        for i in range(n - m + 1):
+            if haystack[i:i + m] == needle:
+                return i
+
+        return -1
+    
+
+sol = Solution()
+
+print(sol.strStr("sadbutsad", "sad"))    # 0
+print(sol.strStr("leetcode", "leeto"))   # -1
