65d36f2e52c18c5a860704e425bead78866dc9fe
Tanujashree-H-V <tanujatanu354@gmail.com>
2026-06-30 21:28:12 +0530

    string formatting

---
+++ string-formatting.py
@@ -0,0 +1,13 @@
+def print_formatted(number):
+    # your code goes here
+     width = len(bin(number)[2:])
+
+     for i in range(1, number + 1):
+        dec = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexa = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+
+        print(dec, octal, hexa, binary)
+
+print_formatted(17)
