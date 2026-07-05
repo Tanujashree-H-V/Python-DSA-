e0bb2bc0e08de5c55ee77cf3cd1439d2a5da7043
Tanujashree-H-V <tanujatanu354@gmail.com>
2026-07-05 21:50:32 +0530

    increament by one

---
+++ plus_one.py
@@ -0,0 +1,21 @@
+class Solution:
+    def plusOne(self, digits):
+        n = len(digits)
+
+        # Traverse from the last digit
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0  # digit was 9, becomes 0
+
+        # If all digits were 9
+        return [1] + digits
+    
+
+s = Solution()
+
+print(s.plusOne([1, 2, 3]))   # [1, 2, 4]
+print(s.plusOne([4, 3, 2, 1])) # [4, 3, 2, 2]
+print(s.plusOne([9]))         # [1, 0]
+print(s.plusOne([9, 9, 9]))   # [1, 0, 0, 0]
