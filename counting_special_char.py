df3bcb869f9473d4556292cd35479ebfbdb409b0
Tanujashree-H-V <tanujatanu354@gmail.com>
2026-07-22 19:29:12 +0530

    counting special character in string

---
+++ counting_special_char.py
@@ -0,0 +1,11 @@
+def count_sp_char(string):
+    sp_char = "!@#$%^&()<>?/\[]{};:~`"
+    count = 0
+    for i in string:
+        if i in sp_char:
+            count+=1
+    return count
+
+text= 'Hello! How are you? #specialchars! 123'
+result= count_sp_char(text)
+print(result)
